5129d8c62025e897795d163a0d1924f47a4334ee
Alexander Lomovskiy <a@lomovskiy.com>
2021-04-07 10:58:41 +0300

    Cleanup

--- app/src/main/java/com/lomovskiy/lib/imagepicker/sample/AppLoader.kt
+++ app/src/main/java/com/lomovskiy/lib/imagepicker/sample/AppLoader.kt
@@ -3,23 +3,8 @@ package com.lomovskiy.lib.imagepicker.sample
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
-import com.lomovskiy.lib.imagepicker.ImageCompressor
-import com.lomovskiy.lib.imagepicker.ImageCompressorImpl
-import com.lomovskiy.lib.imagepicker.ImagePicker
-import com.lomovskiy.lib.imagepicker.ImagePickerImpl
 
 class AppLoader : Application() {
-
-    companion object {
-        lateinit var imageCompressor: ImageCompressor
-        lateinit var imagePicker: ImagePicker
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        imageCompressor = ImageCompressorImpl(this, 1600, 1200, 1 * 1024 * 1024)
-        imagePicker = ImagePickerImpl(this, filesDir.path, imageCompressor)
-    }
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
--- app/src/main/java/com/lomovskiy/lib/imagepicker/sample/MainActivity.kt
+++ app/src/main/java/com/lomovskiy/lib/imagepicker/sample/MainActivity.kt
@@ -17,48 +17,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonCamera: Button
     private lateinit var buttonGallery: Button
 
-    private val imageCompressor: ImageCompressor = AppLoader.imageCompressor
-
-    private val permissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-
-    }
-
-    private val galleryLauncher = registerForActivityResult(PickImageFromGalleryContract) {
-        lifecycleScope.launch {
-            if (it != null) {
-                val str = imageCompressor.compressToBase64(it)
-                showToast(str.length.toString())
-            }
-        }
-    }
-
-    private val cameraLauncher = registerForActivityResult(PickImageFromCameraContract) {
-        lifecycleScope.launch {
-            if (it != null) {
-                val str = imageCompressor.compressToBase64(it)
-                showToast(str.length.toString())
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        permissionsLauncher.launch(arrayOf(
-                Manifest.permission.CAMERA
-        ))
         buttonCamera = findViewById(R.id.button_camera)
         buttonGallery = findViewById(R.id.button_gallery)
-        buttonCamera.setOnClickListener {
-            cameraLauncher.launch(imageCompressor.getNewPhotoUri())
-        }
-        buttonGallery.setOnClickListener {
-            galleryLauncher.launch(null)
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 
 }
