89a325c2733b50eaf2ee52636797070a1c82ae58
Alexander Lomovskiy <a@lomovskiy.com>
2021-04-26 18:26:03 +0300

    Added PickFromCameraContract with embedded uri logic

--- app/src/main/java/com/lomovskiy/lib/imagepicker/sample/MainActivity.kt
+++ app/src/main/java/com/lomovskiy/lib/imagepicker/sample/MainActivity.kt
@@ -1,38 +1,38 @@
 package com.lomovskiy.lib.imagepicker.sample
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.content.pm.ResolveInfo
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.result.contract.ActivityResultContracts
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.lomovskiy.lib.imagepicker.ImageCompressor
 import com.lomovskiy.lib.imagepicker.PickImageFromCameraContract
 import com.lomovskiy.lib.imagepicker.PickImageFromGalleryContract
-import com.lomovskiy.lib.imagepicker.createPhotoUri
 import com.lomovskiy.lib.ui.showToast
-import kotlinx.coroutines.launch
-import java.io.File
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+
+        const val TAG = "MainActivity"
+
+    }
+
     private lateinit var buttonCamera: Button
     private lateinit var buttonGallery: Button
+    private lateinit var buttonReadLatestImage: Button
+    private lateinit var imageView: ImageView
+
+    private var latestImage: Uri? = null
 
     private val launcherPickPhotoFromCamera = registerForActivityResult(PickImageFromCameraContract) {
-        it?.let {
-            print(it)
-        }
+        latestImage = it
     }
 
     private val launcherPickPhotoFromGallery = registerForActivityResult(PickImageFromGalleryContract) {
         it?.let {
-
+            latestImage = it
+            showToast(it.path!!)
         }
     }
 
@@ -41,8 +41,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         buttonCamera = findViewById(R.id.button_camera)
         buttonGallery = findViewById(R.id.button_gallery)
+        buttonReadLatestImage = findViewById(R.id.button_read_latest_image)
+        imageView = findViewById(R.id.image_view)
         buttonCamera.setOnClickListener {
-            launcherPickPhotoFromCamera.launch(createPhotoUri(this, ".jpg"))
+            launcherPickPhotoFromCamera.launch(null)
+        }
+        buttonReadLatestImage.setOnClickListener {
+            contentResolver.delete(latestImage!!, null, null)
         }
     }
 
--- lib/src/main/java/com/lomovskiy/lib/imagepicker/PickImageFromCameraContract.kt
+++ lib/src/main/java/com/lomovskiy/lib/imagepicker/PickImageFromCameraContract.kt
@@ -6,13 +6,17 @@ import android.content.Intent
 import android.net.Uri
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.util.*
 
-object PickImageFromCameraContract : ActivityResultContract<Uri?, Uri?>() {
+object PickImageFromCameraContract : ActivityResultContract<Void?, Uri?>() {
 
     private var fileUri: Uri? = null
 
-    override fun createIntent(context: Context, input: Uri?): Intent {
-        fileUri = input
+    override fun createIntent(context: Context, input: Void?): Intent {
+        fileUri = createPhotoUri(context)
         return Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                 .putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
     }
@@ -23,6 +27,11 @@ object PickImageFromCameraContract : ActivityResultContract<Uri?, Uri?>() {
         } else {
             return null
         }
+    }
+
+    private fun createPhotoUri(context: Context): Uri {
+        val file = File("${ContextCompat.getNoBackupFilesDir(context)}/${UUID.randomUUID()}.jpg")
+        return FileProvider.getUriForFile(context, "${context.packageName}.imagepicker.fileprovider", file)
     }
 
 }
